19b5d7bf9cb0034f3e743951a8c31b1aa3024eb1
87johnboyle <john.hazukisan@gmail.com>
2020-01-25 20:04:27 +0000

    Final

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,5 +4,21 @@ require 'sinatra/reloader'
 number = rand(101)
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params['guess'].to_i
+  msg = too_high?(guess, number)
+  erb :index, :locals => {:number => number, msg:msg}
+end
+
+def too_high?(guess, number)
+  if guess > (number + 5)
+    'Way too high!'
+  elsif guess < (number - 5)
+    'Way too low!'
+  elsif guess > number
+    'Too high!'
+  elsif guess < number
+    'Too low!'
+  else
+    "You guessed it! The SECRET NUMBER is ... #{number}"
+  end
 end
